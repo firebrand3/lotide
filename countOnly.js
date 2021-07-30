87939b7cdd6e27dcb2777d5408295f6d7e2e35d6
Asim <asim.awan@outlook.com>
2021-07-30 13:21:42 +0000

    added alternative code used in compass example

--- countOnly.js
+++ countOnly.js
@@ -9,24 +9,24 @@ const assertEqual = function(actual, expected) {
 
 // allItems: an array of strings that we need to look through
 // itemsToCount: an object specifying what to count
-/*const countOnly = function(allItems, itemsToCount) {
+const countOnly = function(allItems, itemsToCount) {
 
   let results = {};
   let count = 0;
   for (const item of allItems) {
     for (const key in itemsToCount) {
       if (item === key && itemsToCount[key] === true) {
-        results[key] = count++;
-      } else if (item === key && itemsToCount[key] === false) {
+        results[key] += 1 //count++;
+      } /*else if (item === key && itemsToCount[key] === false) {
         results[key] = 1;
-      }
+      }*/
     }
   }
   // return console.log(results);
   return results;
-};*/
+};
 
-const countOnly = function(allItems, itemsToCount) {
+/*const countOnly = function(allItems, itemsToCount) {
   const results = {};
   for (const item of allItems) {
     if (itemsToCount[item]) { 
@@ -38,8 +38,8 @@ const countOnly = function(allItems, itemsToCount) {
     }
   }
   return results;
-}
-
+};
+*/
 
 
 
