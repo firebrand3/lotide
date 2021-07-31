823b1d03ff686c0cb770979782a2ad175fe240e2
Asim <asim.awan@outlook.com>
2021-07-31 01:22:18 +0000

    function that loops over object (provided as argument) and returns the key that has value matching the second argument

---
+++ findKeyByValue.js
@@ -0,0 +1,49 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed:  ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+
+//my solution using the for..in loop; Object.keys method not used
+const findKeyByValue = function(obj, val){
+let result = [];
+  for (const key in obj) {
+  if (obj[key] === val) {
+    return(key);
+  }
+  }
+  return(undefined); 
+}
+
+
+// Solution using the Object.keys method and the for..of loop as suggested in compass
+/*
+const findKeyByValue = function(obj, val){
+  let result = Object.keys(obj);
+  for (const key of result) {
+    if (obj[key] === val) {
+      return key;
+    }
+  }
+    return(undefined);
+  }
+*/
+
+
+// TEST CODE
+console.log(findKeyByValue(bestTVShowsByGenre, "The Wire"))
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+
+console.log(findKeyByValue(bestTVShowsByGenre, "That '70s Show"))
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
