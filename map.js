f9c9a54c44a68bc034b0b06e35d63f2f87cc7c1a
Asim <asim.awan@outlook.com>
2021-08-01 23:30:53 +0000

    created code to understad HOF, callback, assertion

---
+++ map.js
@@ -0,0 +1,42 @@
+const eqArrays = function(arr1, arr2) {
+  let match = "";
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        match = false;
+      } else {
+        match = true;
+      }
+    }
+  } else {
+    match = false;
+  }
+  return [match, arr1, arr2];
+};
+
+const assertArraysEqual = function(match) {
+  //console.log(match[1]);
+  //console.log(match[2]);
+
+  if (match[0]) {
+    console.log(`✅✅✅ ${match[1]} matches ${match[2]}`);
+  } else {
+    console.log(`🛑🛑🛑 ${match[1]} does not match ${match[2]}`);
+  }
+};
+
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+
+
+const results1 = map(words, word => word[0]);
+
+assertArraysEqual(eqArrays(words,results1));
